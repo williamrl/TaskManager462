d4864298f0417ca34b135fed27b46eb0365ee89a
Avik812 <avik812@gmail.com>
2025-04-10 00:32:11 -0400

    basic styling changes

---
+++ task-manager/src/component/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'; // Ensure this path is correct
+
+function Navbar() {
+  return (
+    <div className="navbarrr">
+      <h2 style={{ margin: '0px', padding: '10px', fontFamily: 'Arial'}}>Task Manager</h2>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ task-manager/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Welcome to Dashboard</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
